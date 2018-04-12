b84810671b2e87dfebfc4ec55021e624c1e5f6a4
KevinLeca <leca-kevin@sfr.fr>
2018-04-12 11:09:13 +0200

    Add files via upload

---
+++ Mots-Meles/Placer.h
@@ -0,0 +1,5 @@
+#define N 20
+
+void horizontale(char grille[N][N], char mot[N], int x, int y);
+void verticale(char grille[N][N], char mot[N], int x, int y);
+void diagonale(char grille[N][N], char mot[N], int x, int y);
